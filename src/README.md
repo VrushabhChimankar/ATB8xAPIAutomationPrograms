3872c0ab9a4e04bb993f2f94a2e57ca8f1549b04
SeelaCharu <seelacharu151@gmiail.com>
2024-11-27 20:21:07 +0530

    27th Nov

--- src/README.md
+++ src/README.md
@@ -4,3 +4,9 @@
 - Rest Assured Advanced Concepts
 
 # How to run project?
+- Rest Assured
+- TestNG
+- Allure Report, Extent Report
+- Jackson API
+- Apachr POI
+- Log4j
---
+++ src/test/java/ATB8xVrushabh/APiTest001.java
@@ -0,0 +1,21 @@
+package ATB8xVrushabh;
+
+import io.restassured.RestAssured;
+
+public class APiTest001 {
+    public static void main(String[] args) {
+
+        //https://restful-booker.herokuapp.com/booking/1
+       //Base uri :  https://restful-booker.herokuapp.com
+      //BasePath :   /booking/1
+        RestAssured
+                .given()
+                    .baseUri("https://restful-booker.herokuapp.com")
+                    .basePath("/booking/1")
+                .when()
+                    .get()
+                .then()
+                   .log().all();
+
+    }
+}
